a4dbde9e07343bbff10ce7072e89d0e56c36ce78
jarbasai <jarbasai@mailfence.com>
2021-12-16 17:57:18 +0000

    :tada:

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='selene_api',
+    version='0.0.1',
+    packages=['selene_api'],
+    url='',
+    license='',
+    author='jarbasai',
+    author_email='jarbasai@mailfence.com',
+    description='unofficial api for selene backend'
+)

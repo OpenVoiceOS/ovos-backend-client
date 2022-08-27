395144552fc5e00b63bb09d1745fcf0ebc9bf603
jarbasai <jarbasai@mailfence.com>
2022-08-27 15:35:19 +0100

    manifest

--- selene_api/settings.py
+++ selene_api/settings.py
@@ -1,11 +1,16 @@
 import json
-from copy import deepcopy
-from os.path import join, isfile, dirname
-from os import makedirs
-from ovos_utils.configuration import get_xdg_config_save_path
-from selene_api.api import DeviceApi
-from ovos_utils import camel_case_split
+import os
 import re
+from copy import deepcopy
+from os import makedirs
+from os.path import dirname, expanduser, join, isfile, isdir
+
+from json_database import JsonStorage
+
+from ovos_utils import camel_case_split
+from ovos_utils.configuration import get_xdg_config_save_path, get_xdg_data_save_path, get_xdg_data_dirs
+from ovos_config import Configuration
+from selene_api.api import DeviceApi
 
 
 def get_display_name(skill_name: str):
@@ -225,10 +230,10 @@ class RemoteSkillSettings:
 
                     skill_json[f["name"]] = val
 
-        skill_id = remote_id = data.get("skill_gid") or \
-                   data.get("identifier")  # deprecated
+        remote_id = data.get("skill_gid") or \
+                    data.get("identifier")  # deprecated
 
-        fields = skill_id.split("|")
+        fields = remote_id.split("|")
         skill_id = fields[0]
         if len(fields) > 1 and fields[0].startswith("@"):
             skill_id = fields[1]
@@ -243,6 +248,154 @@ class RemoteSkillSettings:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.generate_meta()
         self.upload()
+        self.store()
+
+
+class SeleneSkillsManifest(JsonStorage):
+    """dict subclass with save/load support
+
+    This dictionary contains the metadata expected by selene backend
+    This data is used to populate entries in selene skill settings page
+
+    """
+
+    def __init__(self, api=None):
+        path = os.path.join(get_xdg_data_save_path(), 'skills.json')
+        super().__init__(path, disable_lock=True)
+        if "skills" not in self:
+            self["skills"] = []
+            self.store()
+        self.api = api or DeviceApi()
+
+    @staticmethod
+    def _get_default_skills_directory(conf=None):
+        """ return default directory to scan for skills
+
+        users can define the data directory in mycroft.conf
+        the skills folder name (relative to data_dir) can also be defined there
+
+        NOTE: folder name also impacts all XDG skill directories!
+
+        {
+            "data_dir": "/opt/mycroft",
+            "skills": {
+                "directory": "skills"
+            }
+        }
+
+        Args:
+            conf (dict): mycroft.conf dict, will be loaded automatically if None
+        """
+        conf = conf or Configuration()
+        data_dir = conf.get("data_dir") or "/opt/mycroft"
+        folder = conf["skills"].get("directory") or "skills"
+
+        skills_folder = f"{data_dir}/{folder}"
+
+        # create folder if needed
+        try:
+            makedirs(skills_folder, exist_ok=True)
+        except PermissionError:  # old style /opt/mycroft/skills not available
+            skills_folder = f"{get_xdg_data_save_path()}/{folder}"
+            makedirs(skills_folder, exist_ok=True)
+
+        return expanduser(skills_folder)
+
+    @staticmethod
+    def _get_skill_directories(conf=None):
+        """ returns list of skill directories ordered by expected loading order
+
+        This corresponds to:
+        - XDG_DATA_DIRS
+        - default directory (see get_default_skills_directory method for details)
+        - user defined extra directories
+
+        Each directory contains individual skill folders to be loaded
+
+        If a skill exists in more than one directory (same folder name) previous instances will be ignored
+            ie. directories at the end of the list have priority over earlier directories
+
+        NOTE: empty folders are interpreted as disabled skills
+
+        new directories can be defined in mycroft.conf by specifying a full path
+        each extra directory is expected to contain individual skill folders to be loaded
+
+        the xdg folder name can also be changed, it defaults to "skills"
+            eg. ~/.local/share/mycroft/FOLDER_NAME
+
+        {
+            "skills": {
+                "directory": "skills",
+                "extra_directories": ["path/to/extra/dir/to/scan/for/skills"]
+            }
+        }
+
+        Args:
+            conf (dict): mycroft.conf dict, will be loaded automatically if None
+        """
+        # the contents of each skills directory must be individual skill folders
+        # we are still dependent on the mycroft-core structure of skill_id/__init__.py
+
+        conf = conf or Configuration()
+        folder = conf["skills"].get("directory") or "skills"
+
+        # load all valid XDG paths
+        skill_locations = list(reversed(
+            [os.path.join(p, folder) for p in get_xdg_data_dirs()]
+        ))
+
+        # load the default skills folder
+        default = SeleneSkillsManifest._get_default_skills_directory(conf)
+        if default not in skill_locations:
+            skill_locations.append(default)
+
+        # load additional explicitly configured directories
+        conf = conf.get("skills") or {}
+        # extra_directories is a list of directories containing skill subdirectories
+        # NOT a list of individual skill folders
+        skill_locations += conf.get("extra_directories") or []
+        return skill_locations
+
+    def device_skill_state_hash(self):
+        return hash(json.dumps(self, sort_keys=True))
+
+    def add_skill(self, skill_id):
+        if self.api.identity.uuid:
+            skill_gid = f'@{self.api.identity.uuid}|{skill_id}'
+        else:
+            skill_gid = f'@|{skill_id}'
+        skill = {
+            "name": skill_id,
+            "origin": "non-msm",
+            "beta": True,
+            "status": 'active',
+            "installed": 0,
+            "updated": 0,
+            "installation": 'installed',
+            "skill_gid": skill_gid
+        }
+        if "skills" not in self:
+            self["skills"] = []
+        self["skills"].append(skill)
+
+    def get_skill_state(self, skill_id):
+        """Find a skill entry in the device skill state and returns it."""
+        for skill_state in self.get('skills', []):
+            if skill_state.get('name') == skill_id:
+                return skill_state
+        return {}
+
+    def scan_skills(self, skill_dirs=None):
+        # TODO - this should go into ovos_utils
+        skill_dirs = skill_dirs or self._get_skill_directories()
+        for directory in skill_dirs:
+            if not isdir(directory):
+                continue
+            for skill_id in os.listdir(directory):
+                skill_init = join(directory, skill_id, "__init__.py")
+                if isfile(skill_init):
+                    self.add_skill(skill_id)
+        # TODO - plugin skills
         self.store()
 
 

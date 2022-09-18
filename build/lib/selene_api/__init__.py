aa33ac2ebb91115df8fea1ac17b7ec088088cac5
jarbasai <jarbasai@mailfence.com>
2022-09-18 19:45:01 +0100

    gitignore

--- build/lib/selene_api/__init__.py
+++
@@ -1,3 +0,0 @@
-from selene_api.api import DeviceApi, WolframAlphaApi, OpenWeatherMapApi, STTApi, GeolocationApi
-from selene_api.cloud import SeleneCloud, SecretSeleneCloud
-from selene_api.settings import RemoteSkillSettings

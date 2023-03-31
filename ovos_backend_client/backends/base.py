fa69e1ab581449ce05e66f5d7bab7206e086d307
jarbasai <jarbasai@mailfence.com>
2023-03-31 17:26:35 +0100

    database api base

--- ovos_backend_client/backends/base.py
+++ ovos_backend_client/backends/base.py
@@ -123,7 +123,8 @@ class AbstractBackend:
         """
         OPEN_WEATHER_MAP_LANGUAGES = (
             "af", "al", "ar", "bg", "ca", "cz", "da", "de", "el", "en", "es", "eu", "fa", "fi", "fr", "gl", "he", "hi",
-            "hr", "hu", "id", "it", "ja", "kr", "la", "lt", "mk", "nl", "no", "pl", "pt", "pt_br", "ro", "ru", "se",
+            "hr", "hu", "metric_id", "it", "ja", "kr", "la", "lt", "mk", "nl", "no", "pl", "pt", "pt_br", "ro", "ru",
+            "se",
             "sk",
             "sl", "sp", "sr", "sv", "th", "tr", "ua", "uk", "vi", "zh_cn", "zh_tw", "zu"
         )
@@ -494,4 +495,238 @@ class AbstractBackend:
                 "isolated_skills": False,
                 "lang": "en-us"}
         """
+        raise NotImplementedError()
+
+    # Database api
+    @abc.abstractmethod
+    def db_list_devices(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_get_device(self, uuid):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_update_device(self, uuid, name=None,
+                         device_location=None, opt_in=False,
+                         location=None, lang=None, date_format=None,
+                         system_unit=None, time_format=None, email=None,
+                         isolated_skills=False, ww_id=None, voice_id=None):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_delete_device(self, uuid):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_post_device(self, uuid, token, name=None,
+                       device_location="somewhere",
+                       opt_in=Configuration.get("opt_in", False),
+                       location=Configuration.get("location"),
+                       lang=Configuration.get("lang"),
+                       date_format=Configuration.get("date_format", "DMY"),
+                       system_unit=Configuration.get("system_unit", "metric"),
+                       time_format=Configuration.get("date_format", "full"),
+                       email=None,
+                       isolated_skills=False,
+                       ww_id=None,
+                       voice_id=None):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_list_shared_skill_settings(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_get_shared_skill_settings(self, skill_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_update_shared_skill_settings(self, skill_id,
+                                        display_name=None,
+                                        settings_json=None,
+                                        metadata_json=None):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_delete_shared_skill_settings(self, skill_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_post_shared_skill_settings(self, skill_id,
+                                      display_name,
+                                      settings_json,
+                                      metadata_json):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_list_skill_settings(self, uuid):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_get_skill_settings(self, uuid, skill_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_update_skill_settings(self, uuid, skill_id,
+                                 display_name=None,
+                                 settings_json=None,
+                                 metadata_json=None):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_delete_skill_settings(self, uuid, skill_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_post_skill_settings(self, uuid, skill_id,
+                               display_name,
+                               settings_json,
+                               metadata_json):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_list_oauth_apps(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_get_oauth_app(self, token_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_update_oauth_app(self, token_id, client_id=None, client_secret=None,
+                            auth_endpoint=None, token_endpoint=None, refresh_endpoint=None,
+                            callback_endpoint=None, scope=None, shell_integration=None):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_delete_oauth_app(self, token_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_post_oauth_app(self, token_id, client_id, client_secret,
+                          auth_endpoint, token_endpoint, refresh_endpoint,
+                          callback_endpoint, scope, shell_integration=True):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_list_oauth_tokens(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_get_oauth_token(self, token_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_update_oauth_token(self, token_id, token_data):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_delete_oauth_token(self, token_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_post_oauth_token(self, token_id, token_data):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_list_stt_recordings(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_get_stt_recording(self, rec_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_update_stt_recording(self, rec_id, transcription=None, metadata=None):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_delete_stt_recording(self, rec_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_post_stt_recording(self, rec_id, byte_data, transcription, metadata=None):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_list_ww_recordings(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_get_ww_recording(self, rec_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_update_ww_recording(self, rec_id, transcription=None, metadata=None):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_delete_ww_recording(self, rec_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_post_ww_recording(self, rec_id, byte_data, transcription, metadata=None):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_list_metrics(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_get_metric(self, metric_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_update_metric(self, metric_id, metadata):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_delete_metric(self, metric_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_post_metric(self, metric_type, metadata):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_list_ww_definitions(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_get_ww_definition(self, ww_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_update_ww_definition(self, ww_id, name, ww_config, plugin):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_delete_ww_definition(self, ww_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_post_ww_definition(self, name, ww_config, plugin):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_list_voice_definitions(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_get_voice_definition(self, voice_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_update_voice_definition(self, voice_id, name=None, lang=None, plugin=None,
+                                   tts_config=None, offline=None, gender=None):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_delete_voice_definition(self, voice_id):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def db_post_voice_definition(self, name, lang, plugin,
+                                 tts_config, offline, gender=None):
         raise NotImplementedError()
